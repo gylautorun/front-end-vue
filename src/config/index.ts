a32e57a6bd0b2b485922831cb0584ea3f1ef601e
ganyanlin <yanlingan_web@163.com>
2025-11-03 15:31:10 +0800

    feat: 大屏可视化比例化 -- echarts 图比例修改 -- 路由白名单

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,13 @@ export const LOGIN_URL: string = '/login';
 export const DEFAULT_PRIMARY: string = '#2449ff';
 
 // * 路由白名单地址（必须是本地存在的路由 staticRouter.ts）
-export const ROUTER_WHITE_LIST: string[] = ['/500', '/data-visualization/big-screen'];
+export const ROUTER_WHITE_LIST: string[] = [
+    '/500',
+    '/404',
+    '/403',
+    '/login',
+    '/data-visualization/big-screen'
+];
 
 export const defaultSettings = {
     language: 'zh-cn',
