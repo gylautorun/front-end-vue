f988b9ad83c37457bfd3b339174a21dd65b5b7ac
gylautorun <16381712+gylautorun@user.noreply.gitee.com>
2026-01-13 17:46:58 +0800

    feat: Sass 星空

---
+++ src/pages/animate-css/sass-star-sky/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="sass-star-sky">
+        <div class="star-sky1"></div>
+        <div class="star-sky2"></div>
+        <div class="star-sky3"></div>
+        <div class="star-sky4"></div>
+        <div class="star-sky5"></div>
+        <div class="star-sky-content">Sass 星空</div>
+    </div>
+</template>
+<script setup lang="ts" name="sass-star-sky">
+import { ref } from 'vue';
+</script>
+
+<style scoped lang="scss">
+@import url('./index.scss');
+</style>
