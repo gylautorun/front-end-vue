a645c8b816d01e5366cd04b5b69bc01f8b371dd9
gylautorun <16381712+gylautorun@user.noreply.gitee.com>
2026-01-19 16:23:04 +0800

    feat: 效果

---
+++ src/pages/animate-css/falling-stars-bg/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="relative flex h-full flex-col items-center justify-center falling-stars-bg">
+        <FallingStarsBg class="bg-white dark:bg-black" :color="isDark ? '#FFF' : '#555'" />
+        <div class="z-[1] flex items-center">
+            <span class="text-6xl font-bold text-black dark:text-white">Inspira UI</span>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import FallingStarsBg from '@/components/inspira-ui/falling-stars-bg.vue';
+import { useColorMode } from '@vueuse/core';
+import { computed } from 'vue';
+
+const isDark = computed(() => useColorMode().value === 'dark');
+</script>
+<style lang="scss" scoped>
+.falling-stars-bg {
+    min-height: 100vh;
+}
+</style>
---
+++ src/pages/animate-css/falling-stars-bg/page.ts
@@ -0,0 +1,10 @@
+export const page = {
+    title: '流星背景',
+    template: 'falling-stars-bg',
+    // 控制布局显示的元数据
+    showSider: false, // 是否隐藏侧边栏
+    showHeader: false, // 是否隐藏顶部导航
+    showFooter: false, // 是否隐藏底部信息
+    isClearLayoutGap: true // 是否清除布局间距
+};
+export default page;
