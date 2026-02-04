d87b544add7acd55931f02b88fab75ef0022dfcb
gylautorun <16381712+gylautorun@user.noreply.gitee.com>
2026-02-04 15:12:40 +0800

    feat:

---
+++ src/pages/css-resize/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- <div class="css-box">
+        该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+        该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+        该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+    </div> -->
+    <!-- <div class="dock-layout-one">
+      <div class="docker">HEADER</div>
+      <div class="main">
+        该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+        该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+        该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+      </div>
+      <div class="docker">FOOTER</div>
+    </div> -->
+    <div class="dock-layout">
+        <div class="docker">
+            <div class="docker-resize"></div>
+            <div class="docker-content">HEADER</div>
+        </div>
+        <div class="main">
+            该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+            该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+            该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。该命令书中的command（AddContent）表明本次消息需要执行的是在光标出插入内容,数据中的content指明本次需要插入的具体内容为“中国”两个汉字。该API没有返回值，所以callback为空。
+        </div>
+        <div class="docker docker-bottom">
+            <div class="docker-resize"></div>
+            <div class="docker-content">FOOTER</div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+@import url('./index.scss');
+</style>
